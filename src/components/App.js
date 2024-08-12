52ca06ade3a085dc71ccb2a1d114de5d200dcf01
Helen <gu-helen04@mail.ru>
2024-08-13 04:19:33 +0500

    f1

---
+++ src/components/App.js
@@ -0,0 +1,47 @@
+/* eslint-disable no-unused-vars */
+import { useState, useEffect } from 'react';
+import '../App.css';
+import Clock from './Clock';
+import Form from './Form';
+import { nanoid } from 'nanoid';
+
+function App() {
+  const [clocks, setClocks] = useState([])
+
+  const addNewClock = (clock) => {
+    setClocks((prevState) => [...prevState, {
+      id: nanoid(),
+      name: clock.name,
+      timeZone: clock.timeZone
+    }]);
+  };
+
+  const deleteClock = (id) => {
+    setClocks(clocks.filter((i) => i.id !== id));
+  };
+
+  return (
+    <div className="wrapper">
+      <Form addNewClock={addNewClock} />
+      <div>
+        {Array.isArray(clocks) && clocks.length > 0
+          ? clocks.map((clock) => {
+            return (
+              <Clock
+                key={clock.id}
+                id={clock.id}
+                name={clock.name}
+                timeZone={clock.timeZone}
+                deleteClock={deleteClock}
+              />
+            );
+          })
+          : <div>
+            <p className="no-clocks">No clocks...</p>
+          </div>}
+      </div>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Clock.js
@@ -0,0 +1,24 @@
+/* eslint-disable no-unused-vars */
+import react, { useState, useEffect } from 'react'
+import { getTime } from './ut'
+
+export default function Clock({ deleteClock, name, id, timeZone }) {
+  const [time, setTime] = useState(getTime(timeZone))
+
+  useEffect(() => {
+    setInterval(() => {
+      setTime(getTime(timeZone))
+    }, 1000)
+  }, )
+
+  return (
+    <div className='clock-wrapper'>
+      <p className='items'>{name} :</p>
+      <p className='items'>{`${time[0]}:${time[1]}:${time[2]}`}</p>
+      <p className="button-delete"
+        onClick={() => deleteClock(id)} >
+        ❌
+      </p>
+    </div>
+  )
+}
